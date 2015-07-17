f9a5ff177a62c9a54e5167cb3323f87f8b5c11ce
mrkkrp <markkarpov@opmbx.org>
2015-07-17 15:23:04 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Missing macro tools
+
+[![License GPL 3](https://img.shields.io/badge/license-GPL_3-green.svg)](http://www.gnu.org/licenses/gpl-3.0.txt)
+
+The package contains classic tools for Emacs Lisp developers who want to
+write macros with convenience.
+
+The following functions and macros are present:
+
+* FIXME
+
+## Installation
+
+Put it on your `load-path` and then add the following at the top of file
+where you would like to use the goodies:
+
+```emacs-lisp
+(require 'mmt)
+```
+
+That's it. Don't forget to include it in your list of dependencies if you
+are writing an Emacs Lisp package:
+
+```emacs-lisp
+;; Package-Requires: ((emacs "24.1") (mmt "0.1.0"))
+```
+
+## API
+
+Coming soon…
+
+## License
+
+Copyright © 2015 Mark Karpov
+
+Distributed under GNU GPL, version 3.
---
+++ mmt.el
@@ -0,0 +1,39 @@
+;;; mmt.el --- Missing macro tools for Emacs Lisp -*- lexical-binding: t; -*-
+;;
+;; Copyright © 2015 Mark Karpov <markkarpov@openmailbox.org>
+;;
+;; Author: Mark Karpov <markkarpov@openmailbox.org>
+;; URL: https://github.com/mrkkrp/mmt
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.4"))
+;; Keywords: macro, emacs-lisp
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation, either version 3 of the License, or (at your
+;; option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+;; Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License along
+;; with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; The package contains classic tools for Emacs Lisp developers who want to
+;; write macros with convenience.
+;;
+;; The following functions and macros are present:
+;;
+;; FIXME
+
+;;; Code:
+
+(provide 'mmt)
+
+;;; mmt.el ends here
