612a24b6297a1a8044640db8077eae07bf3ff274
mrkkrp <markkarpov@opmbx.org>
2015-07-18 00:40:06 +0600

    test changes of ‘mmt--gensym-counter’ too

--- test/mmt-test.el
+++ test/mmt-test.el
@@ -34,10 +34,15 @@
 ;; `mmt-gensym'
 
 (ert-deftest mmt-gensym/equality ()
+  (setq mmt--gensym-counter 0)
   (should-not (eq (mmt-gensym) (mmt-gensym)))
+  (should (= mmt--gensym-counter 2))
   (should-not (eq (mmt-gensym "A") (mmt-gensym "A")))
+  (should (= mmt--gensym-counter 4))
   (should-not (eq (mmt-gensym 5) (mmt-gensym 5)))
-  (should (let ((x (mmt-gensym))) (eq x x))))
+  (should (= mmt--gensym-counter 4))
+  (should (let ((x (mmt-gensym))) (eq x x)))
+  (should (= mmt--gensym-counter 5)))
 
 ;; `mmt-make-gensym-list'
 
