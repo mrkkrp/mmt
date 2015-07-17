60c8612296e0b267cc2e2c5ce64ba192b162dfe9
mrkkrp <markkarpov@opmbx.org>
2015-07-17 16:20:26 +0600

    implemented some things
    Add the following primitives:
    
    * ‘mmt-gensym’
    * ‘mmt-with-gensyms’
    * ‘mmt-with-unique-names’

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Missing macro tools
+# Missing macro tools for Emacs Lisp
 
 [![License GPL 3](https://img.shields.io/badge/license-GPL_3-green.svg)](http://www.gnu.org/licenses/gpl-3.0.txt)
 [![Build Status](https://travis-ci.org/mrkkrp/mmt.svg?branch=master)](https://travis-ci.org/mrkkrp/mmt)
@@ -8,7 +8,9 @@ write macros with convenience.
 
 The following functions and macros are present:
 
-* FIXME
+* `mmt-gensym`
+* `mmt-with-gensyms`
+* `mmt-with-unique-names`
 
 ## Installation
 
@@ -28,7 +30,57 @@ are writing an Emacs Lisp package:
 
 ## API
 
-Coming soon…
+```
+mmt-gensym &optional x
+```
+
+Create and return new uninterned symbol as if by calling `make-symbol`.
+
+The only difference between `mmt-gensym` and `make-symbol` is in how the new
+symbol's name is determined. The name is concatenation of a prefix which
+defaults to `"G"` and a suffix which is decimal representation of a number
+that defaults to the value of `mmt--gensym-counter`.
+
+If `x` is supplied and is a string, then that string is used as a prefix
+instead of `"G"` for this call to `mmt-gensym` only.
+
+If `x` is supplied and is an integer, then that integer is used instead of
+the value of `mmt--gensym-counter` as the suffix for this call of
+`mmt-gensym` only.
+
+If and only if no explicit suffix is supplied `mmt--gensym-counter` is
+incremented after it is used.
+
+----
+
+```
+mmt-with-gensyms names &rest body
+```
+
+Bind each variable in `names` to a unique symbol and evaluate `body`.
+
+Each of `names` must be either a symbol, or of the form:
+
+```
+(symbol string-or-symbol)
+```
+
+Bare symbols appearing in `names` are equivalent to:
+
+```
+(symbol symbol)
+```
+
+The `string-or-symbol` is used as the argument to `mmt-gensym` when
+constructing the unique symbol the named variable will be bound to.
+
+----
+
+```
+mmt-with-unique-names names &rest body
+```
+
+This is an alias for `mmt-with-gensyms`.
 
 ## License
 
--- mmt.el
+++ mmt.el
@@ -5,7 +5,7 @@
 ;; Author: Mark Karpov <markkarpov@openmailbox.org>
 ;; URL: https://github.com/mrkkrp/mmt
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "24.1"))
+;; Package-Requires: ((emacs "24.1") (cl-lib "0.3"))
 ;; Keywords: macro, emacs-lisp
 ;;
 ;; This file is not part of GNU Emacs.
@@ -30,9 +30,69 @@
 ;;
 ;; The following functions and macros are present:
 ;;
-;; FIXME
+;; * mmt-gensym
+;; * mmt-with-gensyms
+;; * mmt-with-unique-names
 
 ;;; Code:
+
+(defvar mmt--gensym-counter 0
+  "This is the counter that `mmt-gensym' uses.")
+
+(defun mmt-gensym (&optional x)
+  "Create and return new uninterned symbol as if by calling `make-symbol'.
+
+The only difference between `mmt-gensym' and `make-symbol' is in
+how the new symbol's name is determined.  The name is
+concatenation of a prefix which defaults to \"G\" and a suffix
+which is decimal representation of a number that defaults to the
+value of `mmt--gensym-counter'.
+
+If X is supplied and is a string, then that string is used as a
+prefix instead of \"G\" for this call to `mmt-gensym' only.
+
+If X is supplied and is an integer, then that integer is used
+instead of the value of `mmt--gensym-counter' as the suffix for
+this call of `mmt-gensym' only.
+
+If and only if no explicit suffix is supplied
+`mmt--gensym-counter' is incremented after it is used."
+  (let ((prefix (if (stringp x) x "G"))
+        (suffix (if (integerp x)
+                    x
+                  (prog1 mmt--gensym-counter
+                    (setq mmt--gensym-counter
+                          (1+ mmt--gensym-counter))))))
+    (make-symbol (format "%s%d" prefix suffix))))
+
+(defmacro mmt-with-gensyms (names &rest body)
+  "Bind each variable in NAMES to a unique symbol and evaluate BODY.
+
+Each of NAMES must be either a symbol, or of the form:
+
+  (SYMBOL STRING-OR-SYMBOL)
+
+Bare symbols appearing in NAMES are equivalent to:
+
+  (SYMBOL SYMBOL)
+
+The STRING-OR-SYMBOL is used as the argument to `mmt-gensym' when
+constructing the unique symbol the named variable will be bound
+to."
+  `(let ,(mapcar (lambda (name)
+                   (cl-destructuring-bind (symbol . prefix)
+                       (if (consp name)
+                           (cons (car name) (cadr name))
+                         (cons name name))
+                     (list symbol
+                           (mmt-gensym
+                            (if (symbolp prefix)
+                                (symbol-name prefix)
+                              prefix)))))
+                 names)
+     ,@body))
+
+(defalias 'mmt-with-unique-names 'mmt-with-gensyms)
 
 (provide 'mmt)
 
