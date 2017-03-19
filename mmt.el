0ccd983df06246ae2370b9fd23e02e7869fc1d56
Allen Li <darkfeline@felesatra.moe>
2017-03-18 22:58:05 -0700

    Remove mmt-gensym
    This is identical to cl-gensym in cl-lib, which ships with Emacs.
    There’s no need to provide duplicate names for this within the Emacs
    Lisp community.

--- mmt.el
+++ mmt.el
@@ -30,7 +30,6 @@
 ;;
 ;; The following functions and macros are present:
 ;;
-;; * mmt-gensym
 ;; * mmt-make-gensym-list
 ;; * mmt-with-gensyms
 ;; * mmt-with-unique-names
@@ -42,25 +41,6 @@
 ;; library Alexandria.
 
 (eval-when-compile (require 'cl-lib))
-
-(defalias 'mmt-gensym 'cl-gensym
-  "Create and return new uninterned symbol as if by calling `make-symbol'.
-
-The only difference between `mmt-gensym' and `make-symbol' is in
-how the new symbol's name is determined.  The name is
-concatenation of a prefix which defaults to \"G\" and a suffix
-which is decimal representation of a number that defaults to the
-value of `cl--gensym-counter'.
-
-If X is supplied and is a string, then that string is used as a
-prefix instead of \"G\" for this call to `mmt-gensym' only.
-
-If X is supplied and is an integer, then that integer is used
-instead of the value of `cl--gensym-counter' as the suffix for
-this call of `mmt-gensym' only.
-
-If and only if no explicit suffix is supplied
-`cl--gensym-counter' is incremented after it is used.")
 
 (defun mmt-make-gensym-list (length &optional x)
   "Return a list of LENGTH gensyms.
